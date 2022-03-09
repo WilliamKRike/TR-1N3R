b01cde85a5da1d7343ad7f4e0a488e9bf0ba81dc
WilliamKRike <rikeisepic@gmail.com>
2022-03-08 19:56:44 -0500

    e

--- commands/skip.js
+++ commands/skip.js
@@ -2,9 +2,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { getVoiceConnection } = require('@discordjs/voice');
 const queue = new Map();
-// TypeScript: import ytdl from 'ytdl-core'; with --esModuleInterop
-// TypeScript: import * as ytdl from 'ytdl-core'; with --allowSyntheticDefaultImports
-// TypeScript: import ytdl = require('ytdl-core'); with neither of the above
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('skip')
@@ -15,11 +12,11 @@ module.exports = {
 		console.log(serverQueue);
 		console.log('push song here');
 
-		serverQueue.songs.push(args);
-		console.log('current queue');
-		console.log(serverQueue);
-		console.log('song being appended to queue');
-		console.log(args);
+		// serverQueue.songs.push(args);
+		// console.log('current queue');
+		// console.log(serverQueue);
+		// console.log('song being appended to queue');
+		// console.log(args);
 		const connection = getVoiceConnection(message.guildId);
 		connection.on('stateChange', (oldState, newState) => {
 			console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
